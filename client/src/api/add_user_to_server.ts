b675df061c75cabd93eec4ba0b627223b6fe301a
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-01 15:08:37 +0000

    User with id, name and creationDate details are aded and saved

--- client/src/api/add_user_to_server.ts
+++ client/src/api/add_user_to_server.ts
@@ -1,14 +1,15 @@
 import { print, prompt } from "../ui/console";
 import { baseUrl } from "./base_url";
+import { User } from "../../../server/src/types/posts.types";
 
-export async function addUserToServer(user: string) {
+export async function addUserToServer(inputUser: User) {
 	try {
 		const result = await fetch(baseUrl + "/api/users/add/", {
 			headers: {
 				"Content-Type": "application/json",
 			},
 			method: "POST",
-			body: JSON.stringify({ user: user }),
+			body: JSON.stringify({ user: inputUser }),
 		});
 
        
