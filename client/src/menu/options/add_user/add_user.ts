03b563b1a37a2a82e28f3a1ff2a8e209e06fdfe4
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-01 15:09:49 +0000

    Added user with Id name and CreationDate. The user is added successfully

--- client/src/menu/options/add_user/add_user.ts
+++ client/src/menu/options/add_user/add_user.ts
@@ -1,15 +1,20 @@
 import { addUserToServer } from "../../../api/add_user_to_server";
 import { states } from "../../../states/states";
 import { clear, print, printNewLine, prompt } from "../../../ui/console";
-
+import { User } from "../../../../../server/src/types/posts.types"; 
 export async function addUser() {
 	clear("yes");
+	/* id: string;
+	name: string;
+	creationDate: Date; */
 
-	const user = await prompt("Which user shall we add? ");
-
+	const id = await prompt("What userId shall we add? ");
+	const name = await prompt("What is the user Name? ");
+	const newUser:User = { id:id, name:name,creationDate:new Date()}
 	printNewLine();
-	print(`📨 Adding User "${user}"...`);
-    const success = await addUserToServer(user);
+
+	print(`📨 Adding User "${id} ${name}"...`);
+    const success = await addUserToServer(newUser);
 
 	if (success === true) print("🥳 User Added successfully!");
 	else print("😵 User NOT received.");
